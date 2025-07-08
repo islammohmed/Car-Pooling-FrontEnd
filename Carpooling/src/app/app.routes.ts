fd2e885943eaa066956e47e7f3f135c3c3a83af2
MirnaMohamed <mirnaattallah00@gmail.com>
2025-07-09 00:08:57 +0300

    add map

--- Carpooling/src/app/app.routes.ts
+++ Carpooling/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { ConfirmEmailComponent } from './components/auth/confirm-email/confirm-email.component';
 import { ProfileComponent } from './profile/profile.component';
+import { MapComponent } from './components/map/map.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'map', component: MapComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'confirm-email', component: ConfirmEmailComponent },
   { path: 'profile', component: ProfileComponent },
